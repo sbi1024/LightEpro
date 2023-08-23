4ebd9827e8d2253d92e63282492c2141b0cd6d24
seoboin <sbi1024@naver.com>
2023-08-23 22:39:23 +0900

    spring security 설정 변경

--- src/main/java/com/example/LightEpro/config/SecurityConfig.java
+++ src/main/java/com/example/LightEpro/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,15 +14,12 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptObject() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                (authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**"))
-                        .permitAll()
-        );
+        // TODO 차후 참고예정 블로그 : https://catsbi.oopy.io/c0a4f395-24b2-44e5-8eeb-275d19e2a536
+        // Spring Security 가 설정된 후 , POST API 정상 호출 불가
+        // csrf 를 기본적으로 체크하기 때문 = csrf disable
+        http.csrf().disable();
         return http.build();
     }
-
 }
